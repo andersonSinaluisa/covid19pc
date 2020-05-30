f5c8e0ec45fb2106a7bc911c89b186bc3c66e66a
adrian <adriancarpiomoran@gmail.com>
2020-05-30 00:17:21 -0500

    Grafico, Pastel

--- Covid19PuntosCriticos/app/src/main/java/com/today/covid_19puntoscriticos/Main/ui/dashboard/DashboardFragment.java
+++ Covid19PuntosCriticos/app/src/main/java/com/today/covid_19puntoscriticos/Main/ui/dashboard/DashboardFragment.java
@@ -2,10 +2,14 @@ package com.today.covid_19puntoscriticos.Main.ui.dashboard;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +17,9 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.Chart;
 import com.github.mikephil.charting.charts.PieChart;
@@ -31,34 +38,67 @@ import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.today.covid_19puntoscriticos.R;
 
-import org.w3c.dom.Text;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 
 public class DashboardFragment extends Fragment {
     private PieChart pieChart;
-    private BarChart barChart;
-    private  String[]months=new String[]{"Enero","Febrero","Marzo","Abril","Mayo"};
-    private  int[]sale=new int[]{17,14,12,10,15};
-    private  int[]colors=new int[]{Color.BLACK,Color.RED,Color.GREEN,Color.BLUE,Color.LTGRAY};
+    //private BarChart barChart;
+    private  String[]months=new String[]{"CONFIRMADOS","MUERTOS","RECUPERADOS",};
+    private  int[]sale=new int[]{17,14,12};
+    private  int[]colors=new int[]{Color.BLACK,Color.RED,Color.GREEN,Color.BLUE,Color.MAGENTA};
+    private DashboardViewModel dashboardViewModel;
 
-
-    private TextView textView;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
+        dashboardViewModel =
+                ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        final TextView textView = root.findViewById(R.id.text_dashboard);
+        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
 
-        textView =(TextView)root.findViewById(R.id.text_dashboard);
-
-        textView.setText(getResources().getString(R.string.textMainDashboard));
-
-        barChart=root.findViewById(R.id.barChart);
+        //barChart=root.findViewById(R.id.barChart);
         pieChart=root.findViewById(R.id.pieChart);
+
         createCharts();
+        //getData();
         return root;
     }
+
+    /*private void getData() {
+        RequestQueue queue= Volley.newRequestQueue(getActivity());
+
+        String url="https://corona.lmao.ninja/v2/all";
+        StringRequest stringRequest= new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                try {
+                    JSONObject jsonObject = new JSONObject(response.toString());
+                    jsonObject.getString("cases");
+                    jsonObject.getString("deaths");
+                    jsonObject.getString("recovered");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("ERROR RESPUESTA",error.toString());
+            }
+        });
+
+        queue.add(stringRequest);
+    }*/
     private Chart getSameChart(Chart chart, String descripcion, int textColor, int background, int animateY){
         chart.getDescription().setText(descripcion);
         chart.getDescription().setTextColor(textColor);
@@ -71,7 +111,8 @@ public class DashboardFragment extends Fragment {
 
     private void legend(Chart chart){
         Legend legend=chart.getLegend();
-        legend.setForm(Legend.LegendForm.CIRCLE);
+        legend.setForm(Legend.LegendForm.DEFAULT);
+
         legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
 
 
@@ -79,39 +120,39 @@ public class DashboardFragment extends Fragment {
         for (int i=0; i<months.length;i++){
             LegendEntry entry= new LegendEntry();
             entry.formColor=colors[i];
-            entry.label=months[i];
+            entry.label= months[i];
             entries.add(entry);
         }
         legend.setCustom(entries);
     }
-    private ArrayList<BarEntry>getBarEntries(){
-        ArrayList<BarEntry> entries =new ArrayList<>();
-        for (int i=0; i< sale.length;i++)
-            entries.add(new BarEntry(i,sale[i]));
-        return entries;
-    }
+    /* private ArrayList<BarEntry>getBarEntries(){
+         ArrayList<BarEntry> entries =new ArrayList<>();
+         for (int i=0; i< sale.length;i++)
+             entries.add(new BarEntry(i,sale[i]));
+         return entries;
+     }*/
     private ArrayList<PieEntry>getPieEntries(){
         ArrayList<PieEntry>entries=new ArrayList<>();
         for (int i=0; i<sale.length;i++)
             entries.add(new PieEntry(sale[i]));
         return entries;
     }
-    private void axisX(XAxis axis){
+    /*private void axisX(XAxis axis){
         axis.setGranularityEnabled(true);
         axis.setPosition(XAxis.XAxisPosition.BOTTOM);
         axis.setValueFormatter(new IndexAxisValueFormatter(months));
         // axis.setEnabled(false);
-    }
-    private void axisLeft(YAxis axis){
+    }*/
+    /*private void axisLeft(YAxis axis){
         axis.setSpaceTop(20);
         axis.setAxisMaximum(0);
         //axis.setGranularity(3);
     }
     private void axisRight(YAxis axis){
         axis.setEnabled(false);
-    }
+    }*/
     private void createCharts(){
-        barChart=(BarChart)getSameChart(barChart, "Series",Color.RED,Color.CYAN,3000);
+        /*barChart=(BarChart)getSameChart(barChart, "Series",Color.RED,Color.CYAN,3000);
         barChart.setDrawGridBackground(true);
         barChart.setDrawBarShadow(true);
         barChart.setData(getBarData());
@@ -119,9 +160,9 @@ public class DashboardFragment extends Fragment {
         axisX(barChart.getXAxis());
         axisLeft(barChart.getAxisLeft());
         axisRight(barChart.getAxisRight());
-        //barChart.getLegend().setEnabled(false);
+        //barChart.getLegend().setEnabled(false);*/
 
-        pieChart=(PieChart)getSameChart(pieChart,"Ventas",Color.GRAY,Color.MAGENTA,3000);
+        pieChart=(PieChart)getSameChart(pieChart,"COVID-19",Color.BLACK,Color.LTGRAY,3000);
         pieChart.setHoleRadius(10);
         pieChart.setTransparentCircleRadius(12);
         pieChart.setData(getPieData());
@@ -133,19 +174,19 @@ public class DashboardFragment extends Fragment {
     private DataSet getData(DataSet dataSet){
         dataSet.setColors(colors);
         dataSet.setValueTextColor(Color.WHITE);
-        dataSet.setValueTextSize(8);
+        dataSet.setValueTextSize(10);
         return dataSet;
     }
-    private BarData getBarData(){
+    /*private BarData getBarData(){
         BarDataSet barDataSet=(BarDataSet)getData(new BarDataSet(getBarEntries(),""));
         barDataSet.setBarShadowColor(Color.GRAY);
         BarData barData = new BarData(barDataSet);
-        barData.setBarWidth(0.40f);
+        barData.setBarWidth(0.45f);
         return barData;
-    }
+    }*/
     private PieData getPieData(){
         PieDataSet pieDataSet=(PieDataSet)getData(new PieDataSet(getPieEntries(),""));
-        pieDataSet.setSliceSpace(1);
+        pieDataSet.setSliceSpace(2);
         pieDataSet.setValueFormatter(new PercentFormatter());
         return new PieData(pieDataSet);
 
